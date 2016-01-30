cf766e6d7350872cb9a5e79b6ed7cc76b3954978
footoredo <footoredo@gmail.com>
2016-01-30 22:25:46 +0800

    Adjusting title styles && Chi&Eng

--- _posts/2016-01-30-Python与MySQL的对接方案.md
+++ _posts/2016-01-30-Python与MySQL的对接方案.md
@@ -18,17 +18,17 @@ pip安装：
     
 这个包先与mysql建立一个connection，再建立cursor对数据库进行操作。
 
-###MySQLdb.connect()
+##MySQLdb.connect()
 
     db = MySQLdb.connect(host="localhost", user="joebob",  passwd="moonpie", db="thangs")
 
-###db.cursor()
+##db.cursor()
     
     cur = db.cursor()
 
 通过这个cursor,可以对连接到的mysql进行操作。
 
-####取数据
+###取数据
 
     cur.execute("SELECT * FROM Writers")
     rows = cur.fetchall()
@@ -41,7 +41,7 @@ Output:
 
 `fetchall()`操作返回的是符合条件的数据的tuple，而每个数据又是由含有各个值的tuple构成的。要取单独一个数据可以用`fetchone()`。
 
-#####Trick
+####Trick
 
 在建立cursor的时候加入参数`MySQLdb.cursors.DictCursor`，即
 
@@ -49,12 +49,12 @@ Output:
 
 可以使数据以dict的形式取回（不过`fetchall()`的结果还是用tuple组织起来的）。
 
-####执行修改
+###执行修改
 
     cur.execute("INSERT INTO Writers(Name) VALUES(%s)", ("Jack London"))
 
 *假如要进行一条修改，并提交到服务器的话，在`execute()`后执行`db.commit()`。*
 
-####Hint
+###Hint
 
 貌似会出现与服务器超时断连的情况，可以使用`db.ping(True)`来测试异常，进行重链。
---
+++ static/text.js
@@ -0,0 +1,45 @@
+var latin = function(char) {
+  return char.charCodeAt(0) < 8192;
+}
+
+var han = function(char) {
+  return char.charCodeAt(0) >= 19968 && char.charCodeAt(0) <= 40869;
+}
+
+var process = function(str) {
+  var ret = "";
+  for (var i = 0; i < str.length - 1; i++) {
+    ret += str[i];
+    if (han(str[i]) && latin(str[i+1]) || han(str[i+1]) && latin(str[i]))
+      ret += " "
+  }
+  ret += str[str.length - 1]
+  return ret;
+}
+
+var sync_marker = function(marker, combine) {
+  marker.style.fontSize = combine.css("font-size")
+  marker.style.top = combine.offset().top + "px";
+  marker.style.left = (combine.offset().left - parseInt(marker.style.fontSize) - 2) + "px";
+  return marker;
+}
+
+window.onload = function() {
+  $(".blog").find("*:not(:has(*))").each(function() {
+    $(this).text(process($(this).text()));
+  });
+  
+  $("h2,h3").each(function() {
+    var marker = document.createElement("div"), combine = $(this);
+    marker.setAttribute("class", "marker");
+    marker.innerText = "#";
+    marker = sync_marker(marker, combine);
+    document.body.appendChild(marker);
+    $(window).resize(function(){marker=sync_marker(marker,combine);});
+    $(marker).click(function(){
+      $('html, body').animate({
+        scrollTop: $(marker).offset().top
+      })
+    });
+  });
+};
